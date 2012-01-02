960db1422c9d6f4ac43e3719adf8aacd1d7d56e2
Frédéric Delorme <frederic.delorme@gmail.com>
2012-01-02 15:55:12 +0100

    create the First test case for our BalanceAccount entity.

---
+++ junitTdd/src/test/java/com/samples/tdd/test/balance/BalanceAccountShouldHaveTo.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.samples.tdd.test.balance;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * testCase for the BalanceAccount Entity.
+ * @author Frédéric Delorme<frederic.delorme@gmail.com>
+ *
+ */
+public class BalanceAccountShouldHaveTo {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
