4b63133bb7dc0fc891e142f0be69279d75113187
Frédéric Delorme <frederic.delorme@gmail.com>
2012-01-21 01:37:03 +0100

    Add first tests cases to BankUser.

--- samples/junitTdd/src/main/java/com/samples/tdd/bank/BankUser.java
+++ samples/junitTdd/src/main/java/com/samples/tdd/bank/BankUser.java
@@ -3,7 +3,9 @@
  */
 package com.samples.tdd.bank;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author FDELORME
@@ -22,5 +24,100 @@ public class BankUser {
 	 * date de naissance de l'utilisateur bancaire.
 	 */
 	public Date birthDate;
+	private List<BankAccount> bankAccountsList;
+
+	/**
+	 * Default Contructor
+	 */
+	public BankUser(){
+		firstname="";
+		lastname="";
+		birthDate=new Date();
+		bankAccountsList=new ArrayList<BankAccount>();
+	}
+	
+	/**
+	 * Parameterized constructor.
+	 * @param firstname
+	 * @param lastname
+	 * @param birthdate
+	 */
+	public BankUser(String firstname, String lastname, Date birthdate){
+		this.firstname=firstname;
+		this.lastname=lastname;
+		this.birthDate=birthdate;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	/**
+	 * @return the birthDate
+	 */
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	/**
+	 * Attach the <code>BankAccount ba</code> to this <code>BankUser</code>.
+	 * @param ba
+	 */
+	public void addBankAccount(BankAccount ba) {
+		this.bankAccountsList.add(ba);		
+	}
+
+	/**
+	 * Return the <code>List&lt;BankAccount&gt;</code> attached to this BankUser..
+	 * @return the list of BankAccount attached to this Bank User.
+	 */
+	public List<BankAccount> getBankAccountList() {
+		return bankAccountsList;
+	}
+
+	/**
+	 * return the <code>BankAccount</code> entity corresponding to the provider <code>accountNumber</code>.
+	 * @param accountNumber to be retrieve on this BankUser.
+	 * @return the corresponding BankAccount. 
+	 */
+	public BankAccount getAccountNumber(String accountNumber) {
+		for (BankAccount item : bankAccountsList) {
+			if(item.getAccountNumber().equals(accountNumber)){
+				return item;
+			}
+		}
+		return null;
+	}
 	
 }
--- samples/junitTdd/src/test/java/com/samples/tdd/test/bank/BankUserShouldHaveTo.java
+++ samples/junitTdd/src/test/java/com/samples/tdd/test/bank/BankUserShouldHaveTo.java
@@ -1,9 +1,18 @@
 package com.samples.tdd.test.bank;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import com.samples.tdd.bank.BankAccount;
 import com.samples.tdd.bank.BankUser;
 
 /**
@@ -33,4 +42,52 @@ public class BankUserShouldHaveTo {
 		bu = null;
 	}
 
+	/**
+	 * Verify if BankUser is clearly initialized.
+	 */
+	@Test
+	public void beInitializedWithFirstNameLastNameBirthDateEmpty(){
+		assertEquals("BankUser has no Firstname initialized","",bu.getFirstname());
+		assertEquals("BankUser has no Lastname initialized","",bu.getLastname());
+		assertEquals("BankUser has no birthDate initialized",new Date().toString(),bu.getBirthDate().toString());
+	}
+	
+	@Test
+	public void beCreatedWithParameters(){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			bu = new BankUser("Frédéric","Delorme",sdf.parse("02/02/1973"));
+		} catch (ParseException e) {
+			fail("Can't parse birthdate on provided string");
+		}
+		
+		assertEquals("BankUser was not intialized with Frédéric","Frédéric",bu.getFirstname());
+		assertEquals("BankUser was not intialized with Delorme","Delorme",bu.getLastname());
+		try {
+			assertEquals("BankUser was not intialized with Frédéric",sdf.parse("02/02/1973"),bu.getBirthDate());
+		} catch (ParseException e) {
+			fail("Can't parse birthdate on provided string");
+		}
+		
+	}
+	
+	@Test
+	public void beLinkedToOneBankAccountAtLeast(){
+		BankAccount ba = new BankAccount(1000.0);
+		bu.addBankAccount(ba);
+		assertEquals("BankUser has not been added with one BankAccount",1, bu.getBankAccountList().size());
+		
+	}
+
+	@Test
+	public void returnOneAccountOnItsAccountNumber(){
+		BankAccount ba = new BankAccount(1000.0);
+		bu.addBankAccount(ba);
+		
+		// retrieve BankAccount on it's account number.
+		BankAccount rep = bu.getAccountNumber(ba.getAccountNumber());
+		
+		assertEquals("BankUser has not been linked to "+ba.getAccountNumber()+" account Number",ba,rep);
+	}
+	
 }
